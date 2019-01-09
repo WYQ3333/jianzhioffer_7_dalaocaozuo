c80374e28dabc1d93375a62f99514bfc91d27ba5
浅忆梦微凉 <3538922590@qq.com>
2019-01-09 22:50:41 +0800

    编译通过

--- 剑指offer7.1_丑数/test.cpp
+++ 剑指offer7.1_丑数/test.cpp
@@ -13,7 +13,7 @@ public:
 		int t2 = 0, t3 = 0, t5 = 0, i;
 		for (i = 1; i < index; ++i)
 		{
-			res[i] = min(res[t2] * 2, res[t3] * 3, res[t5] * 5);
+			res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
 			if (res[i] == res[t2] * 2)t2++;
 			if (res[i] == res[t3] * 3)t3++;
 			if (res[i] == res[t5] * 5)t5++;
